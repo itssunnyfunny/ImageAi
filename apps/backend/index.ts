cb02446a3292ef7fb42e29a7686552777cf9000e
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-03-21 11:48:27 +0530

    adding endpoints to the backend

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.post("/ai/training", (req, res)=> {
+
+})
+app.post("/ai/generate", (req, res)=> {
+    
+})
+app.post("/pack/generate", (req, res)=> {
+    
+})
+app.get("/pack/bulk", (req, res)=> {
+    
+})
+
+app.get("/image", (req, res)=> {
+    
+})
+
+app.listen(PORT, ()=> {
+    console.log("Server is running on port 8080");
+    
+})
