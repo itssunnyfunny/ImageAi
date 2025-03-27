5798465ae801be69bce8b97cb6c25155598c4c14
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-03-27 11:38:46 +0530

    adding flux ai api key and client and adding and baseModel

---
+++ apps/backend/Models/BaseModel.ts
@@ -0,0 +1,11 @@
+
+
+export class BaseModel {
+    constructor() { }
+
+    private async generateImage(prompt: string, tenserPath: string ) {}
+
+    private async trainModel(inputImages : string[],triggerWord: string) {}
+
+}
+
