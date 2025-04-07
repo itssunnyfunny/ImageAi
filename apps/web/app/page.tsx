1505a18d4239bdc109d88926f880b47c385e8811
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-04-07 13:56:10 +0530

    adding shadcn with npx need fix in bun

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,13 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+
 import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      just a test
-      <h1 className="text-blue-500 text-4xl">hello jee</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
