d4882fcf3cd8774d71d84d9c49ba64bb2c8254ba
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-05-18 10:56:19 +0530

    deleting locally

--- apps/backend/Models/BaseModel.ts
+++ apps/backend/Models/BaseModel.ts
@@ -3,7 +3,7 @@
 export  class BaseModel {
     constructor() { }
 
-    private async generateImage(prompt: string, tenserPath: string ) {}
+   private async generateImage(prompt: string, tenserPath: string ) {}
 
     private async trainModel(inputImages : string[],triggerWord: string) {}
 
